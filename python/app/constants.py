0537c125bc0cc046a2d482197b2804471be6df13
Trey Cucco <tcucco@churchcommunitybuilder.com>
2020-10-23 11:22:28 -0600

    fix a docstring typo

--- python/app/constants.py
+++ python/app/constants.py
@@ -1,4 +1,4 @@
-"""This module containts constants used throughout the application."""
+"""This module contains constants used throughout the application."""
 APP_BASE_URL = "http://localhost:8080"
 OAUTH_BASE_URL = "https://oauth.ccbchurch.com"
 API_BASE_URL = "https://api.ccbchurch.com"
